6d389d5fe05f5f35beac418778435119988c4a4c
Vinicius Bento <vinicius_bento10@hotmail.com>
2023-08-02 16:31:32 -0300

    OneBitCode

--- JavaScript/exercicio-4/cidades.js
+++ JavaScript/exercicio-4/cidades.js
@@ -5,7 +5,7 @@ let quantidadeCidades = 0
 while (visitouOuNao == true) {
     visitouOuNao = confirm("Você visitou alguma outra nova cidade?")
     if (visitouOuNao == true) {
-        nomeCidade += prompt("Qual nome da cidade?") + "\n"
+        nomeCidade += "- " + prompt("Qual nome da cidade?") + "\n"
         quantidadeCidades++
     } else {
         alert(`Você visitou no total ${quantidadeCidades} cidades!\n E as cidades visitadas foram :\n ${nomeCidade}`)
