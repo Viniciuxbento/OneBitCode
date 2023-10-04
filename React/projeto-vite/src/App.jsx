2bbb1f1482c2f99c7fc5b42750950e890fabbeb4
Vinicius Bento <vinicius_bento10@hotmail.com>
2023-10-04 11:08:56 -0300

    Commit

---
+++ React/projeto-vite/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App(){
+  return(<div>
+    <img src="/react.png" alt="" />
+    <h1>Fandango</h1>
+  
+  
+  
+  
+  
+  </div>
+    
+  )
+}
---
+++ React/reactEx01/src/App.jsx
@@ -0,0 +1,13 @@
+function sum(a,b){
+  return a*b
+}
+
+export default function App(){
+  return(
+    <div>
+      <h1>OLAA MUNDO</h1>
+      <h2>retornara o valor {sum(16,9)}</h2>
+    </div>
+    
+  )
+}
